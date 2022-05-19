1bf1cd3d811e9a5b0575f77bbae295388ba41fc1
Jovana Agobian <jovana.agobian@gmail.com>
2022-05-19 11:56:11 -0400

    commit tree 0c6d7c0e2dde2a08914eca86e99f2d9c79fc03d0

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,17 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Dancer
+    extend FancyDance::ClassMethods
+    include FancyDance::ClassMethods
+    include Dance
+    extend MetaDancing
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,15 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+
+class Kid
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+    include Dance
+    extend MetaDancing
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end
