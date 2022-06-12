6235a7cb4cb8223e2145787b4ce329ebc1319736
Daryl Rogony <darylrogony@gmail.com>
2022-06-12 12:04:04 +0300

    commit tree e85421eb7f74d1f7145d78ec9230e6e9bf8dac8f

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,14 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Dancer
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,13 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+
+class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
