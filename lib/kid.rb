beb640104ced12260902114bd74ca0c97355b7a2
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-08-20 10:31:23 +0300

    commit tree c410e8331940f905354203e463419039372eea5c

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,14 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance.rb'
+
+class Kid
+    include Dance
+    extend MetaDancing
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
